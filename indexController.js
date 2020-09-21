d3b8bc8ce03e11670333fb7f3d1f537547d35f81
Karlo Topić <ktopic00@fesb.hr>
2020-09-21 14:03:37 +0200

    v1.0

---
+++ indexController.js
@@ -0,0 +1,29 @@
+var ourApp=angular.module("chatbot",[
+    "ngRoute",
+    "ngResource",
+    "pascalprecht.translate"
+]);
+ourApp.controller('indexCtrl', [ "$scope", "$resource", function($scope, $resource) {
+
+    function init() {
+        $scope.appName = "GoToBot";
+        $scope.email = "";
+        $scope.message = "";
+    }
+
+    init();
+
+    $scope.addUser = function() {
+        var controller = "backend/registerUser.php";
+        var emailResponse = $resource(controller + '?email=' + $scope.email);
+        emailResponse.get(function(data) {
+            $scope.email = "";
+            if (data.response) {
+                $scope.message = "SUBSCRIBE_OK";
+            } else {
+                $scope.message = "SUBSCRIBE_OK";
+            }
+        });
+    }
+
+}]);
